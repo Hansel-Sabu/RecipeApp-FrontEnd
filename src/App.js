f52ade046385c4d6499573566a836d13aebb8897
Hansel-Sabu <er.hanselsabu25@gmail.com>
2024-04-05 23:02:30 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewRecipes from './components/ViewRecipes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <AddRecipe/>
-      <SearchRecipe/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/'element={<ViewRecipes/>}/>
+        <Route path='/1'element={<AddRecipe/>}/>
+        <Route path='/2'element={<SearchRecipe/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/ViewRecipes.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewRecipes = () => {
+  return (
+    <div>
+      <Navbar/>
+         <div className="container">
+                          <div className="row">
+                              <div className="col">
+                                  <div className="col-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                      <div className="row g-3">
+
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/30/32/45/19L2ucjQneoEANPzyfiQ_0S9A6363.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Oven Baked Ribs</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/30/97/7/7GmgWw4TTUCCw7xGKQAP_0S9A6434.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Mozerlle chips</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/28/18/77/ZyG0h2VYRjei7iPFSWNI_food.com-14.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Shakshuka</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/63/78/6/NrPa79ZESEOqMlMoFDos_fajitas-3.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Fajitas</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/48/66/41/aB9Ar2MzTy2CIyxoINma_b.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Banana Nut bread</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/40/49/7/iUlxb54sSWaD9Zp44kfP_FGgWFV4mSVq8aISp1eQG_baked%2520wings%2520(1%2520of%25204).jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Oven Crisp Chicken</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/16/52/54/XE7h7mpTK69lpb05OhGB_broil-perfect-steak_4984.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Boiled Steak</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                          <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 col-xxl-3">
+                                              <div class="card">
+                                                  <img src="https://img.sndimg.com/food/image/upload/f_auto,c_thumb,q_55,w_500,ar_4:3/v1/img/recipes/11/70/31/ieo2HguHQEiy7VYnh9cT_0S9A5095.jpg" class="card-img-top" alt="..." />
+                                                  <div class="card-body">
+                                                      <p class="fs-4">Brocolli and Garlic Pasta</p>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+    </div>
+  )
+}
+
+export default ViewRecipes
