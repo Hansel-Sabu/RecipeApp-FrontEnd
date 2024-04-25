4554cf894afb678f9b505368e8c4b703b997c748
Hansel-Sabu <er.hanselsabu25@gmail.com>
2024-04-25 15:18:13 +0530

    Commited

--- src/components/SearchRecipe.jsx
+++ src/components/SearchRecipe.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const SearchRecipe = () => {
-  return (
-    <div>
-        <Navbar/>
-        <container>
-            <div className="row">
-                <div className="col col-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3">
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col">
                         <div className="col-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">Title</label>
-                        <input type="text" className="form-control" />
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Search</button>
+                            <div className="row g-3">
+                                <div className="col-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                                    <label htmlFor="" className="form-label">Title</label>
+                                    <input type="text" className="form-control" />
+                                </div>
+                                <div className="col-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                    <button className="btn btn-success">Search</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </container>
-    </div>
+        </div>
   )
 }
 
